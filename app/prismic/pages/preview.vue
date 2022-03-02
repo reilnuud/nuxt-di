9d99420571093af40d3c20ac258b58b224a1d7cd
Neil Ruud <neil@ruudmedia.com>
2022-03-02 14:06:02 -0800

    fix preview page location

---
+++ app/prismic/pages/preview.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-grow items-center justify-center text-white bg-purple">
+    <p class="animate-pulse">Loading preview...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$prismic.preview();
+  }
+};
+</script>
